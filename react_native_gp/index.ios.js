ad5e47de89d91e3838974ab5d4089d1850a9a1d6
shiqiangdva <1114005726@qq.com>
2018-12-04 13:23:21 +0800

    navigator实现页面之间跳转以及数据传递与回调

--- react_native_gp/index.ios.js
+++ react_native_gp/index.ios.js
@@ -9,11 +9,13 @@ import {
     AppRegistry,
     StyleSheet,
     Text,
+    Navigator,
     Image,
     View
 } from 'react-native';
 // 引入tab组件
 import TabNavigator from 'react-native-tab-navigator';
+import Boy from './Boy';
 
 export default class react_native_gp extends Component {
 
@@ -28,52 +30,69 @@ export default class react_native_gp extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <TabNavigator>
-                    <TabNavigator.Item
-                        selected={this.state.selectedTab === 'tb_popular'}
-                        selectedTitleStyle={{color:'red'}}
-                        title="最热"
-                        renderIcon={() => <Image style={styles.image} source={require('./res/image/ic_polular.png')}/>}
-                        renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'red'}]} source={require('./res/image/ic_polular.png')}/>}
-                        badgeText="1"
-                        onPress={() => this.setState({selectedTab: 'tb_popular'})}>
-                        {/*{homeView}*/}
-                        <View style={styles.page1}></View>
-                    </TabNavigator.Item>
-                    <TabNavigator.Item
-                        selected={this.state.selectedTab === 'tb_trending'}
-                        selectedTitleStyle={{color:'yellow'}}
-                        title="趋势"
-                        renderIcon={() => <Image style={styles.image} source={require('./res/image/ic_trending.png')}/>}
-                        renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'yellow'}]} source={require('./res/image/ic_trending.png')}/>}
-                        // renderBadge={() => <CustomBadgeView/>}
-                        onPress={() => this.setState({selectedTab: 'tb_trending'})}>
-                        {/*{profileView}*/}
-                        <View style={styles.page2}></View>
-                    </TabNavigator.Item>
-                    <TabNavigator.Item
-                        selected={this.state.selectedTab === 'tb_favorite'}
-                        selectedTitleStyle={{color:'red'}}
-                        title="收藏"
-                        renderIcon={() => <Image style={styles.image} source={require('./res/image/ic_polular.png')}/>}
-                        renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'red'}]} source={require('./res/image/ic_polular.png')}/>}
-                        badgeText="1"
-                        onPress={() => this.setState({selectedTab: 'tb_favorite'})}>
-                        {/*{homeView}*/}
-                        <View style={styles.page1}></View>
-                    </TabNavigator.Item>
-                    <TabNavigator.Item
-                        selected={this.state.selectedTab === 'tb_my'}
-                        selectedTitleStyle={{color:'yellow'}}
-                        title="我的"
-                        renderIcon={() => <Image style={styles.image} source={require('./res/image/ic_trending.png')}/>}
-                        renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'yellow'}]} source={require('./res/image/ic_trending.png')}/>}
-                        // renderBadge={() => <CustomBadgeView/>}
-                        onPress={() => this.setState({selectedTab: 'tb_my'})}>
-                        {/*{profileView}*/}
-                        <View style={styles.page2}></View>
-                    </TabNavigator.Item>
-                </TabNavigator>
+                {/*<TabNavigator>*/}
+                    {/*<TabNavigator.Item*/}
+                        {/*selected={this.state.selectedTab === 'tb_popular'}*/}
+                        {/*selectedTitleStyle={{color:'red'}}*/}
+                        {/*title="最热"*/}
+                        {/*renderIcon={() => <Image style={styles.image} source={require('./res/image/ic_polular.png')}/>}*/}
+                        {/*renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'red'}]} source={require('./res/image/ic_polular.png')}/>}*/}
+                        {/*badgeText="1"*/}
+                        {/*onPress={() => this.setState({selectedTab: 'tb_popular'})}>*/}
+                        {/*/!*{homeView}*!/*/}
+                        {/*<View style={styles.page1}></View>*/}
+                    {/*</TabNavigator.Item>*/}
+                    {/*<TabNavigator.Item*/}
+                        {/*selected={this.state.selectedTab === 'tb_trending'}*/}
+                        {/*selectedTitleStyle={{color:'yellow'}}*/}
+                        {/*title="趋势"*/}
+                        {/*renderIcon={() => <Image style={styles.image} source={require('./res/image/ic_trending.png')}/>}*/}
+                        {/*renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'yellow'}]} source={require('./res/image/ic_trending.png')}/>}*/}
+                        {/*// renderBadge={() => <CustomBadgeView/>}*/}
+                        {/*onPress={() => this.setState({selectedTab: 'tb_trending'})}>*/}
+                        {/*/!*{profileView}*!/*/}
+                        {/*<View style={styles.page2}></View>*/}
+                    {/*</TabNavigator.Item>*/}
+                    {/*<TabNavigator.Item*/}
+                        {/*selected={this.state.selectedTab === 'tb_favorite'}*/}
+                        {/*selectedTitleStyle={{color:'red'}}*/}
+                        {/*title="收藏"*/}
+                        {/*renderIcon={() => <Image style={styles.image} source={require('./res/image/ic_polular.png')}/>}*/}
+                        {/*renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'red'}]} source={require('./res/image/ic_polular.png')}/>}*/}
+                        {/*badgeText="1"*/}
+                        {/*onPress={() => this.setState({selectedTab: 'tb_favorite'})}>*/}
+                        {/*/!*{homeView}*!/*/}
+                        {/*<View style={styles.page1}></View>*/}
+                    {/*</TabNavigator.Item>*/}
+                    {/*<TabNavigator.Item*/}
+                        {/*selected={this.state.selectedTab === 'tb_my'}*/}
+                        {/*selectedTitleStyle={{color:'yellow'}}*/}
+                        {/*title="我的"*/}
+                        {/*renderIcon={() => <Image style={styles.image} source={require('./res/image/ic_trending.png')}/>}*/}
+                        {/*renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'yellow'}]} source={require('./res/image/ic_trending.png')}/>}*/}
+                        {/*// renderBadge={() => <CustomBadgeView/>}*/}
+                        {/*onPress={() => this.setState({selectedTab: 'tb_my'})}>*/}
+                        {/*/!*{profileView}*!/*/}
+                        {/*<View style={styles.page2}></View>*/}
+                    {/*</TabNavigator.Item>*/}
+                {/*</TabNavigator>*/}
+
+                <Navigator
+                    initialRoute={{
+                        component: Boy
+                    }}
+                    renderScene={(route, navigator) => {
+                        /**
+                         * 这里注意!!!
+                         * Component是从route.component中取出来的 不是react中的
+                         * 不然会报错:inst.render is not a function
+                         */
+                        let Component = route.component;
+                        return <Component navigator={navigator} {...route.params}/>
+                    }}
+                >
+
+                </Navigator>
             </View>
         );
     }
